9f6aab2eef227009bb2f3797a933232396ed342f
Leviathan Douglas <douglas@mines.edu>
2025-10-30 10:47:28 -0600

    Added missing functions and arguments Also added more comments

--- Player.hpp
+++ Player.hpp
@@ -15,12 +15,22 @@ class Player {
     virtual ~Player() = default;
     
 
-    virtual void draw() = 0;
+    /// \param modelMtx existing model matrix to apply to Caedilas
+    /// \param viewMtx camera view matrix to apply to Caedilas
+    /// \param projMtx camera projection matrix to apply to Caedilas
+    /// \note internally uses the provided shader program and sets the necessary uniforms
+    /// for the MVP and Normal Matrices as well as the material diffuse color
+    virtual void draw(const glm::mat4& viewMtx, const glm::mat4& projMtx ) const = 0;
     virtual void animate(const GLfloat dTime) = 0;
 
     virtual void moveForward(const GLfloat speed) final;
     virtual void moveBackward(const GLfloat speed) final;
     virtual void rotate(const GLfloat dTheta, const GLfloat dPhi) final;
+
+    // set world boundaries (assumed to be symmetrical from 0,0,0)
+    virtual void setWorldEdges(const GLfloat x, const GLfloat y, const GLfloat z) final {
+      _worldEdges = glm::vec3(x, y, z);
+    }
 
     // setters and getters
     virtual GLfloat getPhi() const final { return mPhi; }
@@ -30,6 +40,10 @@ class Player {
     virtual void setTheta(const GLfloat t) final { mTheta = t; _computeOrientation(); }
     virtual void setPosition(const glm::vec3 pos) final {mPosition = pos; _computeOrientation(); }
 
+    /// \param shaderProgramHandle shader program handle that the Caedilas should be drawn using
+    /// \param mvpMtxUniformLocation uniform location for the full precomputed MVP matrix
+    /// \param normalMtxUniformLocation uniform location for the precomputed Normal matrix
+    /// \param materialColorUniformLocation uniform location for the material diffuse color
     virtual void setProgramUniformLocations( GLuint shaderProgramHandle, GLint mvpMtxUniformLocation, GLint normalMtxUniformLocation ) final {
     mShaderProgramHandle = shaderProgramHandle;
     mShaderProgramUniformLocations = { mvpMtxUniformLocation, normalMtxUniformLocation };
@@ -38,9 +52,15 @@ class Player {
   private:
     glm::vec3 _worldEdges;
     void _clampPhi();
+    // \desc keep player inside of world boundaries
     void _computeOrientation();
 
   protected:
+    // helpers
+    static constexpr GLfloat s_PI = glm::pi<float>();
+    static constexpr GLfloat s_2PI = glm::two_pi<float>();
+    static constexpr GLfloat s_PI_OVER_2 = glm::half_pi<float>();
+
     Player();
     glm::vec3 mPosition;
     glm::vec3 mDirection; // normalized direction vector
@@ -49,9 +69,13 @@ class Player {
 
     virtual void mComputeAndSendMatrixUniforms(const glm::mat4& modelMtx, const glm::mat4& viewMtx, const glm::mat4& projMtx) const;
 
+    /// \desc handle of the shader program to use when drawing
     GLuint mShaderProgramHandle;
+    /// \desc stores the uniform locations needed
     struct ShaderProgramUniformLocations {
+      /// \desc location of the precomputed ModelViewProjection matrix
       GLint mpvMtx;
+      /// \desc location of the precomputed Normal matrix
       GLint normalMtx;
     } mShaderProgramUniformLocations;
 };
